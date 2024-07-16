21a7ec961e7152f6212fb5b0d1759aea6073c51d
Bautista <baucanepa@gmail.com>
2024-07-16 14:36:45 -0300

    Edicion de models.py acorde a la base de datos - Desarrollo del HTML: Form Footer y Header - Creacion de About.html y su Footer y Header

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-cfrom flask import Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,26 +7,29 @@ class Contenido(db.Model):
     __tablename__= 'contenidos'
     id= db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
-    genero = db.Column(db.String(100), db.ForeignKey('generos.id'))
-    fecha_lunch = db.Column(db.Integer, nullable = False)
-    donde_ver = db.Column(db.String(100), db.ForeignKey('plataformas.id'))
+    genero = db.Column(db.Integer, db.ForeignKey('generos.id_genero'))
+    fecha_lunch = db.Column(db.Date, nullable = False)
+    donde_ver = db.Column(db.Integer, db.ForeignKey('plataformas.id_plataforma'))
     kids = db.Column(db.Boolean)
-    peli_o_serie = db.column (db.String(50),db.ForeignKey('pelis_o_series.id'))
+    peli_o_serie = db.column (db.Integer,db.ForeignKey('pelis_o_series.id_peli_o_serie'))
 
 
 class Genero(db.Model):
     __tablename__= 'generos'
-    id= db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
+    id_genero= db.Column(db.Integer, primary_key=True)
+    nombre_genero = db.Column(db.String(100), nullable=False)
     
 class Plataforma(db.Model):
     __tablename__= 'plataformas'
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    cuota_mensual = db.Column(db.Integer, nullable = False)
+    id_plataforma = db.Column(db.Integer, primary_key=True)
+    nombre_plataforma = db.Column(db.String(100), nullable=False)
 
 class Peli_o_serie(db.Model):
     __tablename__= 'pelis_o_series'
-    id= db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    
+    id_peli_o_serie= db.Column(db.Integer, primary_key=True)
+    nombre_peli_o_serie = db.Column(db.String(100), nullable=False)
+
+class Estado(db.Model):
+    __tablename__='estados'
+    id_estado=db.Column(db.Integer, primary_key=True)
+    nombre_estado=db.column(db.String(100), nullable=False)
