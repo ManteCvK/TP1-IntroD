65206b65d2456dc2cd745efc1b5e628530aebee1
Emmagiovine <emmig2005@gmail.com>
2024-07-22 21:27:15 -0300

    desarrollo del metodo GET para kids, peliculas, series y lista con sus respectivos filtros

--- backend/app.py
+++ backend/app.py
@@ -78,11 +78,12 @@ def nuevo_contenido():
         nuevo_tipo = data.get ('tipo')
         nueva_imagen = data.get('imagen')
         kids = data.get('kids')
-        if kids == 0:
+
+        if kids == '0':
             nuevo_kids = False
-        else:
+        elif kids == '1':
             nuevo_kids = True
-            
+    
         nuevo_estado = '2'
         nuevo_contenido = Contenido(id=id, nombre=nuevo_nombre, genero = nuevo_genero, fecha_lunch = nueva_fecha_lunch, 
         donde_ver = nueva_plataforma, peli_o_serie = nuevo_tipo, kids = nuevo_kids, imagen = nueva_imagen, estado=nuevo_estado), 201
@@ -119,10 +120,12 @@ def modificar_todo_contenido(id_contenido):
         nuevo_tipo = data.get ('tipo')
         nueva_imagen = data.get('imagen')
         kids = data.get('kids')
-        if kids == 0:
+
+        if kids == '0':
             nuevo_kids = False
-        else:
+        elif kids == '1':
             nuevo_kids = True
+
         contenido.nombre = nuevo_nombre
         contenido.peli_o_serie = nuevo_tipo
         contenido.genero = nuevo_genero
@@ -134,6 +137,99 @@ def modificar_todo_contenido(id_contenido):
         return {"success": True}
     except:
      return {"success": False}
+    
+@app.route("/kids", methods = ["GET"])
+def contenidos_kids():
+    try: 
+        contenidos_kids = Contenido.query.where(Contenido.kids == True).all()
+        contenidos_data_kids = []
+        for kid in contenidos_kids:
+            contenido_data = {
+                'id':kid.id,
+                'nombre':kid.nombre,
+                'genero':kid.genero,
+                'fecha de lanzamiento': kid.fecha_lunch,
+                'plataforma': kid.donde_ver,
+                'tipo': kid.peli_o_serie,
+                'kids': kid.kids,
+                'imagen': kid.imagen,
+                'estado' : kid.estado,
+            }
+          
+            contenidos_data_kids.append(contenido_data)
+        return (jsonify(contenidos_data_kids))
+    except:
+        return(jsonify("No hay elementos"))    
+    
+@app.route("/peliculas", methods = ["GET"])
+def contenidos_pelis():
+    try: 
+        contenidos_pelis= Contenido.query.where(Contenido.peli_o_serie == 0).all()
+        contenidos_data_pelis = []
+        for peli in contenidos_pelis:
+            contenido_data = {
+                'id':peli.id,
+                'nombre':peli.nombre,
+                'genero':peli.genero,
+                'fecha de lanzamiento': peli.fecha_lunch,
+                'plataforma': peli.donde_ver,
+                'tipo': peli.peli_o_serie,
+                'kids': peli.kids,
+                'imagen': peli.imagen,
+                'estado' : peli.estado,
+            }
+          
+            contenidos_data_pelis.append(contenido_data)
+        return (jsonify(contenidos_data_pelis))
+    except:
+        return(jsonify("No hay elementos"))    
+    
+@app.route("/series", methods = ["GET"])
+def contenidos_series():
+    try: 
+        contenidos_series= Contenido.query.where(Contenido.peli_o_serie == 1).all()
+        contenidos_data_series = []
+        for serie in contenidos_series:
+            contenido_data = {
+                'id':serie.id,
+                'nombre':serie.nombre,
+                'genero':serie.genero,
+                'fecha de lanzamiento': serie.fecha_lunch,
+                'plataforma': serie.donde_ver,
+                'tipo': serie.peli_o_serie,
+                'kids': serie.kids,
+                'imagen': serie.imagen,
+                'estado' : serie.estado,
+            }
+          
+            contenidos_data_series.append(contenido_data)
+        return (jsonify(contenidos_data_series))
+    except:
+        return(jsonify("No hay elementos")) 
+    
+@app.route("/lista", methods = ["GET"])
+def contenidos_lista():
+    try: 
+        contenidos_lista= Contenido.query.where(Contenido.estado != 2).all()
+        contenidos_data_lista = []
+        for i in contenidos_lista:
+            contenido_data = {
+                'id':i.id,
+                'nombre':i.nombre,
+                'genero':i.genero,
+                'fecha de lanzamiento': i.fecha_lunch,
+                'plataforma': i.donde_ver,
+                'tipo': i.peli_o_serie,
+                'kids': i.kids,
+                'imagen': i.imagen,
+                'estado' : i.estado,
+            }
+          
+            contenidos_data_lista.append(contenido_data)
+        return (jsonify(contenidos_data_lista))
+    except:
+        return(jsonify("No hay elementos")) 
+    
 
 if __name__ == '__main__':
     print('starting server...')
